11ddd8a22af2ee17c2d6ba28a99234438f26afdf
ryuta.hayashi <ryuta.hayashi@fujifilm.com>
2023-04-17 19:01:35 +0900

    add test code.

--- src/sub1.rs
+++ src/sub1.rs
@@ -1,4 +1,5 @@
 use std::{io::Read};
+use anyhow::{Context, Result};
 
 struct PicoEntry<'a, T> {
     dat: Vec<&'a [T]>,
@@ -54,10 +55,10 @@ impl<'a, T> PicoTts<'a, T>
         x
     }
 
-    pub fn from_path<'b>(path: &std::ffi::OsStr) -> std::io::Result<PicoTts<'b, u8>> {
+    pub fn from_path(path: &std::ffi::OsStr) -> Result<PicoTts<'_, u8>> {
         let mut v = std::fs::File::open(path)?;
         let mut buf = vec![];
-        let _ = v.read_to_end(&mut buf)?;
+        let _ = v.read_to_end(&mut buf).context("unexpect read to file.")?;
         Ok(PicoTts {
             sys: buf,
             sub: Vec::new(),
@@ -100,4 +101,88 @@ impl<'a, T> PicoTts<'a, T>
         self.init()
     }
 
+}
+
+#[cfg(test)]
+mod tests {
+    use std::vec;
+    use crate::sub1::*;
+
+    #[test]
+    // 目的：PicoTts::new()関数が正しく動作するかを確認する
+    fn test_pico_tts_new() {
+        let pico_tts: PicoTts<u8> = PicoTts::new();
+        assert_eq!(pico_tts.sys, vec![]);
+        assert_eq!(pico_tts.sub, Vec::<&[u8]>::new());
+        assert_eq!(pico_tts.subsub, None);
+    }
+    
+    #[test]
+    // 目的：PicoTts::new_unsafe()関数が正しく動作するかを確認する
+    fn test_pico_tts_new_unsafe() {
+        let pico_tts = unsafe { PicoTts::<'_, u8>::new_unsafe() };
+        assert_eq!(pico_tts.sys, vec![1, 2, 3, 4, 5]);
+        assert_eq!(pico_tts.sub.len(), 4);
+        assert_eq!(pico_tts.subsub, None);
+    }
+    
+    #[test]
+    // 目的：PicoTts::new_with_data()関数が正しく動作するかを確認する
+    fn test_pico_tts_new_with_data() {
+        let data = vec![1, 2, 3, 4, 5];
+        let pico_tts = PicoTts::new_with_data(&data);
+        assert_eq!(pico_tts.sys, vec![1, 2, 3, 4, 5]);
+        assert_eq!(pico_tts.sub.len(), 4);
+        assert_eq!(pico_tts.subsub.unwrap().len(), 4);
+    }
+    
+    #[test]
+    // 目的：PicoTts::from_path()関数が正しく動作するかを確認する
+    fn test_pico_tts_from_path() {
+        let path = std::ffi::OsStr::new("Cargo.toml");
+        let pico_tts = PicoTts::<'_, u8>::from_path(&path).unwrap();
+        assert_eq!(pico_tts.sys.len(), 226);
+        assert_eq!(pico_tts.sub.len(), 0);
+        assert_eq!(pico_tts.subsub, None);
+    }
+    
+    #[test]
+    // 目的：PicoTts::init()関数が正しく動作するかを確認する
+    fn test_pico_tts_init() {
+        let mut pico_tts = PicoTts::new_with_data(&vec![1, 2, 3, 4, 5]);
+        let pico_tts = pico_tts.init();
+        assert_eq!(pico_tts.sys, vec![1, 2, 3, 4, 5]);
+        assert_eq!(pico_tts.sub.len(), 8);
+        assert_eq!(pico_tts.subsub.as_ref().unwrap().len(), 8);
+    }
+    
+    #[test]
+    // 目的：PicoTts::get_sub()関数が正しく動作するかを確認する
+    fn test_pico_tts_get_sub() {
+        let pico_tts = PicoTts::new_with_data(&vec![1, 2, 3, 4, 5]);
+        let sub = pico_tts.get_sub(2);
+        let test: &[i32] = &[3, 4];
+        assert_eq!(sub, Some(test));
+    }
+    
+    #[test]
+    // 目的：PicoTts::update_sub()関数が正しく動作するかを確認する
+    fn test_pico_tts_update_sub() {
+        let mut pico_tts = PicoTts::new_with_data(&vec![1, 2, 3, 4, 5]);
+        let dat: Vec<&[i32]> = vec![&[1, 2], &[3, 4], &[5, 6]];
+        pico_tts.update_sub(dat);
+        let test: Vec<&[i32]> = vec![&[1, 2], &[3, 4], &[5, 6]];
+        assert_eq!(pico_tts.subsub.as_ref().unwrap(), &test);
+    }
+    
+    #[test]
+    // 目的：PicoTts::update_sys()関数が正しく動作するかを確認する
+    fn test_pico_tts_update_sys() {
+        let mut pico_tts = PicoTts::new_with_data(&vec![1, 2, 3, 4, 5]);
+        let data = vec![6, 7, 8, 9, 10];
+        let pico_tts = pico_tts.update_sys(&data);
+        assert_eq!(pico_tts.sys, vec![6, 7, 8, 9, 10]);
+        assert_eq!(pico_tts.sub.len(), 4);
+        assert_eq!(pico_tts.subsub.as_ref().unwrap().len(), 4);
+    }
 }
